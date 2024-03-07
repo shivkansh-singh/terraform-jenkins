0f4634e25f0abaf9a37e20ec2d285281c64d9ba1
shivkansh-singh <161964686+shivkansh-singh@users.noreply.github.com>
2024-03-07 20:41:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 
+provider "aws" {
+    region = "ap-south-1"  
+}
+
 
 resource "aws_instance" "linux" {
   ami           = "ami-0a0f1259dd1c90938"
@@ -6,7 +10,7 @@ resource "aws_instance" "linux" {
   key_name      = "new_key_pair"
 
   tags = {
-    Name = "HelloWorld-${count.index}"
+    Name = "HelloWorld"
   }
-  count = 1
+  
 }
