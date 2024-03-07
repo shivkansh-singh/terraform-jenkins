c74f62a784908340c0fa36622dac4e47eefcb832
shivkansh-singh <161964686+shivkansh-singh@users.noreply.github.com>
2024-03-07 18:01:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,5 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  region = "ap-south-1"
+  access_key = " AKIA2GWTQB7NNHP5HKMF"
+  secret_key = "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 "
 }
