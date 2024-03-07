62eacc6ea88090ee3dd32a44cf64516601107523
shivkansh-singh <161964686+shivkansh-singh@users.noreply.github.com>
2024-03-07 17:44:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=5.31.0"
-    }
-  }
-}
+
 
 resource "aws_instance" "linux" {
   ami           = "ami-0a0f1259dd1c90938"
